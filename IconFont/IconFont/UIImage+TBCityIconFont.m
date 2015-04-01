a51db65b0390128ed3478cfab7c0719ee3068df6
John Wong <huangxiaozhe1988@gmail.com>
2015-04-01 23:52:03 +0800

    0.0.2

--- IconFont/IconFont/UIImage+TBCityIconFont.m
+++ IconFont/IconFont/UIImage+TBCityIconFont.m
@@ -21,10 +21,18 @@
     CGContextRef context = UIGraphicsGetCurrentContext();
  
     if ([info.text respondsToSelector:@selector(drawAtPoint:withAttributes:)]) {
+        /**
+         * 如果这里抛出异常，请打开断点列表，右击All Exceptions -> Edit Breakpoint -> All修改为Objective-C
+         * See: http://stackoverflow.com/questions/1163981/how-to-add-a-breakpoint-to-objc-exception-throw/14767076#14767076
+         */
         [info.text drawAtPoint:CGPointZero withAttributes:@{NSFontAttributeName:font, NSForegroundColorAttributeName: info.color}];
     } else {
+        
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         CGContextSetFillColorWithColor(context, info.color.CGColor);
         [info.text drawAtPoint:CGPointMake(0, 0) withFont:font];
+#pragma clang pop
     }
     
     UIImage *image = [UIImage imageWithCGImage:UIGraphicsGetImageFromCurrentImageContext().CGImage scale:scale orientation:UIImageOrientationUp];
